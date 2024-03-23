6140b4da1b3407ee5c40316f4512d63f588f46c2
Alma Broman <alma.broman@gmail.com>
2024-03-23 16:40:13 +0000

    Add Root routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/register" render={() => <h1>Register</h1>} />
+          <Route render={() => <h1>Page not found! :o</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
