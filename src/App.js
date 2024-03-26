adbcc42e380afd052212f172144b0c4749561bdd
Alma Broman <alma.broman@gmail.com>
2024-03-26 14:29:19 +0000

    Add sign in function and current user context custom context hook

--- src/App.js
+++ src/App.js
@@ -4,22 +4,42 @@ import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
+
+
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>} />
-          <Route exact path="/explore" render={() => <h1>Explore</h1>} />
-          <Route exact path="/search" render={() => <h1>Search</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-          <Route exact path="/register" render={() => <h1><SignUpForm /></h1>} />
-          <Route render={() => <h1>Page not found! :o</h1>} />
-        </Switch>
-      </Container>
-    </div>
+
+        <div className={styles.App}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={() => <h1>Home Page</h1>} />
+              <Route exact path="/explore" render={() => <h1>Explore</h1>} />
+              <Route exact path="/search" render={() => <h1>Search</h1>} />
+              <Route
+                exact
+                path="/signin"
+                render={() => (
+                  <h1>
+                    <SignInForm />
+                  </h1>
+                )}
+              />
+              <Route
+                exact
+                path="/register"
+                render={() => (
+                  <h1>
+                    <SignUpForm />
+                  </h1>
+                )}
+              />
+              <Route render={() => <h1>Page not found! :o</h1>} />
+            </Switch>
+          </Container>
+        </div>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,30 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
+  const currentUser = useCurrentUser();
+  const loggedInIcons = <>{currentUser?.username}</>
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/register"
+      >
+        <i className="fas fa-user-plus"></i>Register
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-right-to-bracket"></i>Sign In
+      </NavLink>
+    </>
+  );
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container className={styles.NavContainer}>
@@ -16,46 +38,33 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <div className={styles.NavLinksContainer}>
-          <Nav className="text-left flex-column">
-            <NavLink
-              exact
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/"
-            >
-              <i className="fas fa-house-chimney"></i>Home
-            </NavLink>
-            <NavLink
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/explore"
-            >
-              <i className="fas fa-user-astronaut"></i>Explore
-            </NavLink>
-            <NavLink
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/search"
-            >
-              <i className="fas fa-magnifying-glass"></i>Search
-            </NavLink>
-          </Nav>
-          <Nav className="mt-5 text-left flex-column">
-          <NavLink
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/register"
-            >
-              <i className="fas fa-user-plus"></i>Register
-            </NavLink>
-            <NavLink
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/signin"
-            >
-              <i className="fas fa-right-to-bracket"></i>Sign In
-            </NavLink>
-          </Nav>
+            <Nav className="text-left flex-column">
+              <NavLink
+                exact
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/"
+              >
+                <i className="fas fa-house-chimney"></i>Home
+              </NavLink>
+              <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/explore"
+              >
+                <i className="fas fa-user-astronaut"></i>Explore
+              </NavLink>
+              <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/search"
+              >
+                <i className="fas fa-magnifying-glass"></i>Search
+              </NavLink>
+            </Nav>
+            <Nav className="mt-5 text-left flex-column">
+              {currentUser ? loggedInIcons : loggedOutIcons}
+            </Nav>
           </div>
         </Navbar.Collapse>
       </Container>
