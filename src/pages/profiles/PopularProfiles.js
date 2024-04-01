3acdf0cb934a3541302a93f44b6ef6388a9f8f93
Alma Broman <alma.broman@gmail.com>
2024-04-01 16:53:14 +0000

    Add context provider for profile data

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -5,32 +5,10 @@ import { axiosReq } from '../../api/axiosDefaults';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Asset from '../../components/Asset';
 import Profile from '../posts/Profile';
+import { useProfileData } from '../../contexts/ProfileDataContext';
 
 const PopularProfiles = () => {
-    const [profileData, setProfileData] = useState({
-        pageProfile: {results: []},
-        popularProfiles: {results: []},
-    });
-    const { popularProfiles } = profileData;
-    const currentUser = useCurrentUser()
-
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const {data} = await axiosReq.get(
-                    "/profiles/?ordering=-followers_count"
-                );
-                setProfileData((prevState) => ({
-                    ...prevState,
-                    popularProfiles: data,
-                }));
-            } catch(err){
-                console.log(err)
-            }
-        };
-
-        handleMount()
-    }, [currentUser]);
+    const { popularProfiles } = useProfileData();
 
   return (
     <Container className={`${appStyles.Content} ${appStyles.Border} text-center`}>
